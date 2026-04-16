637e5d7f6a826558a0670a18c140b04a8c1ab113
Sheetal <sheetalyd02@gmail.com>
2026-04-16 12:22:44 +0530

    demo project

---
+++ app.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  fs.writeFileSync('file.txt', 'Backup + Cloud working!');
+  
+  res.send('App running with Docker + Cloud!');
+});
+
+app.listen(PORT, () => console.log("Running"));
